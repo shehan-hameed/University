f7e8ccc7f09eda9408ec2c36ba4017f6a15fb394
shehan-hameed <shehan2005savain@gmail.com>
2026-06-19 19:32:27 +0530

    Shipping cost calculate using Javascript

--- Structured Programming/Javascript/yt.js
+++ Structured Programming/Javascript/yt.js
@@ -1,15 +1,32 @@
-function calculateShippingCost() {
-    let totalPrice = 19;
+const standardShippingCost = 6 // CamelCase (the way we name variables/function etc)
+const distcountedShippingCost = 4
+let totalPrice;
+
+function calculateShippingCost(totalPriceParameter) {
     let shippingCost;
 
     if (totalPrice <= 10) {
-        shippingCost = 5
+        shippingCost = standardShippingCost // less hard coded variables
     } else if (totalPrice <= 20) {
-        shippingCost = 3
+        shippingCost = standardShippingCost // less hard coded variables
     } else {
         shippingCost = 0
     }
     console.log(`Shipping cost for you is ${shippingCost}`)
+    console.log(`for total price of ${totalPrice}`)
+    console.log("-----------------------")
 }
+// user starts shopping
+totalPrice = 10
+calculateShippingCost(totalPrice) // ******
 
-calculateShippingCost()
+// more items added to shopping cart
+totalPrice = 19
+// ready to order
+calculateShippingCost(totalPrice) //******
+
+// add another item
+totalPrice = 24;
+calculateShippingCost(totalPrice) // ******
+
+// place the order
